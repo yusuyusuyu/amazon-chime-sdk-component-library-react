a57aa669a7ccc7cf66006c2912b171d3d330a422
yusuyusuyu <112844897+yusuyusuyu@users.noreply.github.com>
2022-09-05 07:17:56 +0200

    Update main.tsx

--- .storybook/main.tsx
+++ .storybook/main.tsx
@@ -1,32 +1,30 @@
-const path = require('path');
+<ThemeProvider theme={darkTheme}>
+  <Roster>
+    <RosterHeader
+      title="Present"
+      badge={2}
+      onClose={() => {}}
+      searchValue="Michael"
+      onSearch={() => {}}
+    />
+    <RosterGroup>
+      <RosterCell
+        name="Michael Scott"
+        subtitle="Regional Manager"
+        muted={false}
+        videoEnabled={true}
+      />
+      <RosterCell
+        name="Michael Scarn"
+        subtitle="FBI agent"
+        muted={true}
+        videoEnabled={false}
+      />
+    </RosterGroup>
+    <RosterGroup title="Left" badge={2}>
+      <RosterCell name="Dwight" subtitle="Assistant regional manager" />
+      <RosterCell name="Mike the Magic" subtitle="Magician" />
+    </RosterGroup>
+  </Roster>
+</ThemeProvider>
 
-module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
-  },
-  stories: ['../src/**/*.stories.@(ts|js|tsx|mdx)'],
-  addons: [
-    '@storybook/addon-knobs',
-    '@storybook/addon-storysource',
-    '@storybook/addon-viewport',
-    'themeprovider-storybook',
-    '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-        babelOptions: {},
-        sourceLoaderOptions: null,
-      },
-    },
-  ],
-};
